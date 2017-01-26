8cb54de7d8ee7476caa4965514335d8c67cc1d4b
pgilmorepf <paul@playfab.com>
2017-01-26 10:26:05 -0800

    Updating the TypeScript example to use strong types for most of the Cloud Script inputs and results.
    Summary:
    Updating the TypeScript example to use strong types for most of the Cloud Script inputs and results.
    Removing unused parts of the default VS example.
    
    Test Plan: Manual for the moment
    
    Reviewers: Marco.Williams
    
    Maniphest Tasks: T2184
    
    Differential Revision: https://phab.playfabdev.com/D3139

--- ExampleCloudScript.js
+++ ExampleCloudScript.js
@@ -1,50 +1,37 @@
 ///////////////////////////////////////////////
 // JenkinsConsoleUtility CloudScript functions
 ///////////////////////////////////////////////
-
-var TEST_TITLE_ID: string = "6195"; // NOTE: Replace this with your own titleID - DeleteUsers has an additional security check to avoid accidents
-var TEST_DATA_KEY: string = "TEST_DATA_KEY"; // Used to reuse args.customId, but it was kindof a pain, and made things fragile
-
-handlers.helloWorld = function (args: IHelloWorldRequest, context): IHelloWorldResult {
-    var message: string = "Hello " + currentPlayerId + "!";
+var TEST_TITLE_ID = "6195"; // NOTE: Replace this with your own titleID - DeleteUsers has an additional security check to avoid accidents
+var TEST_DATA_KEY = "TEST_DATA_KEY"; // Used to reuse args.customId, but it was kindof a pain, and made things fragile
+handlers.helloWorld = function (args, context) {
+    var message = "Hello " + currentPlayerId + "!";
     log.info(message);
-    var inputValue: any = null;
+    var inputValue = null;
     if (args && args.hasOwnProperty("inputValue"))
         inputValue = args.inputValue;
     log.debug("helloWorld:", { input: inputValue });
     return { messageValue: message };
 };
-interface IHelloWorldRequest {
-    inputValue?: any
-}
-interface IHelloWorldResult {
-    messageValue: string
-}
-
-handlers.throwError = function (args: any): void {
-    var testObject: any = undefined;
-    var failureObj: any = testObject.doesnotexist.doesnotexist;
+handlers.throwError = function (args) {
+    var testObject = undefined;
+    var failureObj = testObject.doesnotexist.doesnotexist;
     return failureObj; // Can't get to here
-}
-
-handlers.easyLogEvent = function (args): void {
+};
+handlers.easyLogEvent = function (args) {
     log.info(JSON.stringify(args.logMessage));
 };
-
 ///////////////////////////////////////////////
 // JenkinsConsoleUtility CloudScript functions
 ///////////////////////////////////////////////
-
-handlers.TestDataExists = function (args: any): boolean {
+handlers.TestDataExists = function (args) {
     var playerData = server.GetUserInternalData({
         PlayFabId: currentPlayerId,
         Keys: [TEST_DATA_KEY]
     });
     return playerData.Data.hasOwnProperty(TEST_DATA_KEY);
 };
-
-handlers.GetTestData = function (args: any): ITestReport {
-    var testResults: ITestReport = null;
+handlers.GetTestData = function (args) {
+    var testResults = null;
     var playerData = server.GetUserInternalData({
         PlayFabId: currentPlayerId,
         Keys: [TEST_DATA_KEY]
@@ -52,31 +39,25 @@ handlers.GetTestData = function (args: any): ITestReport {
     if (playerData.Data.hasOwnProperty(TEST_DATA_KEY)) {
         log.info("Returning Data: " + playerData.Data[TEST_DATA_KEY].Value);
         testResults = JSON.parse(playerData.Data[TEST_DATA_KEY].Value);
-        var data: { [key: string]: string } = {};
+        var data = {};
         data[TEST_DATA_KEY] = null;
         server.UpdateUserInternalData({
             PlayFabId: currentPlayerId,
             Data: data
         });
-    } else {
+    }
+    else {
         log.info("Expected data not found in: " + JSON.stringify(playerData));
     }
-
     return testResults;
 };
-
-handlers.SaveTestData = function (args: ITestReportPackage): void {
-    var data: { [key: string]: string } = {};
+handlers.SaveTestData = function (args) {
+    var data = {};
     data[TEST_DATA_KEY] = JSON.stringify(args.testReport);
     log.info("Saving Data (" + currentPlayerId + "): " + JSON.stringify(data));
     server.UpdateUserInternalData({
         PlayFabId: currentPlayerId,
         Data: data
     });
-}
-interface ITestReportPackage {
-    testReport: ITestReport
-}
-interface ITestReport {
-    // Actually very big/complicated, but not inspected at all in this code.
-}
+};
+//# sourceMappingURL=ExampleCloudScript.js.map
