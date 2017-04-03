0ad501f0ac1a51f73548978d9a7c0cc12c2ce880
pgilmorepf <paul@playfab.com>
2017-04-03 15:52:18 -0700

    Some more examples from newest guides.

--- OtherExamples.ts
+++ OtherExamples.ts
@@ -41,7 +41,7 @@ handlers.SellItem = function (args: ISellItemArgs) {
 
 // Publisher data Examples
 const PUBLISHER_USED_TITLES_KEY = "playedTitleIds";
-handlers.TrackTitleUsage = function() {
+handlers.TrackTitleUsage = function () {
     // Get the User Publisher Data for this player, and convert it into our expected format
     var getRequest: PlayFabServerModels.GetUserDataRequest = { Keys: [PUBLISHER_USED_TITLES_KEY], PlayFabId: currentPlayerId };
     var getResult: PlayFabServerModels.GetUserDataResult = server.GetUserPublisherInternalData(getRequest);
@@ -60,4 +60,42 @@ handlers.TrackTitleUsage = function() {
     playedTitlesList.push(script.titleId);
     var setRequest: PlayFabServerModels.UpdateUserDataRequest = { PlayFabId: currentPlayerId, Data: { PUBLISHER_USED_TITLES_KEY: JSON.stringify(playedTitlesList) } };
     server.UpdateUserPublisherInternalData(setRequest);
+}
+
+const PUBLISHER_REDEEMED_TITLES_KEY = "redeemedTitleIds";
+const MY_CROSS_TITLE_REWARDS: { [key: string]: number } = { "AU": 10 };
+handlers.CheckCrossTitleRewards = function () {
+    // Get the publisher data concerning cross-title rewards for this player
+    var getRequest: PlayFabServerModels.GetUserDataRequest = { Keys: [PUBLISHER_USED_TITLES_KEY, PUBLISHER_REDEEMED_TITLES_KEY], PlayFabId: currentPlayerId };
+    var getResult: PlayFabServerModels.GetUserDataResult = server.GetUserPublisherInternalData(getRequest);
+    var redeemedTitleRewards: { [key: string]: Array<string> } = JSON.parse(getResult.Data[PUBLISHER_REDEEMED_TITLES_KEY].Value); // format is arbitrary, but this example assumes { [key: string]: Array<string> }
+    if (!redeemedTitleRewards)
+        redeemedTitleRewards = {};
+    var playedTitlesList: Array<string> = JSON.parse(getResult.Data[PUBLISHER_USED_TITLES_KEY].Value); // format is arbitrary, but this example assumes Array<string>
+    if (!playedTitlesList)
+        playedTitlesList = [];
+
+    // Determine which titles are un-redeemed
+    var unredeemedTitleIds: Array<string> = [];
+    for (var i = 0; i < playedTitlesList.length; i++) {
+        if (!redeemedTitleRewards.hasOwnProperty(playedTitlesList[i]) && playedTitlesList[i] !== script.titleId)
+            unredeemedTitleIds.push(playedTitlesList[i]);
+    }
+    if (unredeemedTitleIds.length === 0)
+        return null; // Nothing to redeem
+
+    // Give the cross title rewards
+    var multiplier: number = unredeemedTitleIds.length;
+    var actualRewards: { [key: string]: number } = {}; // MY_CROSS_TITLE_REWARDS is a global constant, so don't modify it or you'll mess up future calls
+    for (var eachKey in MY_CROSS_TITLE_REWARDS) {
+        actualRewards[eachKey] = MY_CROSS_TITLE_REWARDS[eachKey] * multiplier;
+        server.AddUserVirtualCurrency({ PlayFabId: currentPlayerId, VirtualCurrency: eachKey, Amount: MY_CROSS_TITLE_REWARDS[eachKey] }); // Can only add 1 VC at a time
+    }
+    // Save the Publisher data indicating rewards are claimed
+    redeemedTitleRewards[script.titleId] = playedTitlesList;
+    var setRequest: PlayFabServerModels.UpdateUserDataRequest = { PlayFabId: currentPlayerId, Data: { PUBLISHER_REDEEMED_TITLES_KEY: JSON.stringify(redeemedTitleRewards) } };
+    server.UpdateUserPublisherInternalData(setRequest);
+
+    // Tell the client the reward
+    return actualRewards;
 }
