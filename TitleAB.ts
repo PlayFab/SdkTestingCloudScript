561ed88c012fa5e8ffbf52dba79ade49c2863949
pgilmorepf <paul@playfab.com>
2017-06-20 15:21:37 -0700

    AB Test TitleData script revised

--- TitleAB.ts
+++ TitleAB.ts
@@ -1,11 +1,23 @@
-﻿var TITLE_AB_TEST_TITLE_KEY = "TitleDataAbTestSegmentIds";
+// Special key in the Title Data that contains an array of AB buckets that participate in the testing
+var TITLE_AB_TEST_TITLE_KEY = "TitleDataAbTestSegmentIds";
 
-handlers.GetTitleDataAB = function (args, ctx): { [key: string]: string } {
+handlers.GetTitleDataAB = function (args, ctx): string {
+    // The data key the player originally requested.
     var dataKey: string = args.TitleKey;
+
+    // A variable to store AB segment of the player, if any
     var currentAbTestSegmentId: string = null;
 
-    var abTestSegmentIds: string[] = JSON.parse(server.GetTitleData({ Keys: [TITLE_AB_TEST_TITLE_KEY] })[TITLE_AB_TEST_TITLE_KEY]);
+    /*
+     * We store a list of bucket IDs that participate in the AB testing in the title data.
+     * This line extracts an array of such ids
+     */
+    var requestedTitleData = server.GetTitleData({ Keys: [TITLE_AB_TEST_TITLE_KEY, dataKey] });
+    var defaultValue: string = requestedTitleData.Data.hasOwnProperty(dataKey) ? requestedTitleData.Data[dataKey] : null;
+    var segmentIdJson: string = requestedTitleData.Data.hasOwnProperty(TITLE_AB_TEST_TITLE_KEY) ? requestedTitleData.Data[TITLE_AB_TEST_TITLE_KEY] : null;
+    var abTestSegmentIds: string[] = JSON.parse(segmentIdJson);
 
+    // This line extracts all the segments current player belongs to
     var playerSegments = server.GetPlayerSegments({ PlayFabId: currentPlayerId }).Segments;
 
     // Locate first ABTest segment the player belongs to
@@ -15,16 +27,23 @@ handlers.GetTitleDataAB = function (args, ctx): { [key: string]: string } {
             currentAbTestSegmentId = playerSegmentId;
     }
 
-    //If player does not belong to any tested segment, return 
+    // If player does not belong to any tested segment, return a value for the original key
     if (!currentAbTestSegmentId)
-        return server.GetTitleData({ Keys: [dataKey] }).Data;
+        return defaultValue;
 
-    // If player belongs to AB tested segment
+    /*
+     * If player belongs to one of AB tested segments
+     * we use ID of this segment to construct special key
+     * First part of this key is the original key
+     * Followed by underscore ('-') we add a suffix, which is ID of the bucket the player belongs to.
+     */
     var abTestedKey: string = dataKey + "_" + currentAbTestSegmentId;
+
+    // We try to get a value using our special key
     var result = server.GetTitleData({ Keys: [abTestedKey] });
 
-    if (result.Data[abTestedKey]) // If there is a/b tested data
-        return result.Data;
-    else //if no a/b tested data defined
-        return server.GetTitleData({ Keys: [dataKey] }).Data;
+    if (result.Data[abTestedKey]) // if we have data defined for this bucket, we return it
+        return result.Data[abTestedKey];
+    else // Otherwise, we return the value for the original key
+        return defaultValue;
 }
