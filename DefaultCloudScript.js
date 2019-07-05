cd1e8835071407bc308c9b8e5808a543edd1100c
Hamza Lazaar <brice2k5@gmail.com>
2019-07-05 12:28:53 +0100

    CHANGED: playstream events replacing useless logs

--- DefaultCloudScript.js
+++ DefaultCloudScript.js
@@ -221,36 +221,126 @@ handlers["unlockHighSkillContent"] = UnlockHighSkillContent;
 // This makes it easier than ever to incorporate multiplayer server logic into your game.
 // Triggered automatically when a Photon room is first created
 var RoomCreated = function (args) {
-    log.debug("Room Created - Game: " + args.GameId + " MaxPlayers: " + args.CreateOptions.MaxPlayers);
+    server.WritePlayerEvent({
+        EventName: "room_created",
+        PlayFabId: args.UserId,
+        Body: {
+            WebHook: {
+                AppVersion: args.AppVersion,
+                Region: args.Region,
+                GameId: args.GameId,
+                Type: args.Type,
+                ActorNr: args.ActorNr,
+                CreateOptions: args.CreateOptions
+            }
+        }
+    });
 };
 handlers["RoomCreated"] = RoomCreated;
 // Triggered automatically when a player joins a Photon room
 var RoomJoined = function (args) {
-    log.debug("Room Joined - Game: " + args.GameId + " PlayFabId: " + args.UserId);
+    server.WritePlayerEvent({
+        EventName: "room_joined",
+        PlayFabId: args.UserId,
+        Body: {
+            WebHook: {
+                AppVersion: args.AppVersion,
+                Region: args.Region,
+                GameId: args.GameId,
+                ActorNr: args.ActorNr
+            }
+        }
+    });
 };
 handlers["RoomJoined"] = RoomJoined;
 // Triggered automatically when a player leaves a Photon room
 var RoomLeft = function (args) {
-    log.debug("Room Left - Game: " + args.GameId + " PlayFabId: " + args.UserId);
+    server.WritePlayerEvent({
+        EventName: "room_left",
+        PlayFabId: args.UserId,
+        Body: {
+            WebHook: {
+                AppVersion: args.AppVersion,
+                Region: args.Region,
+                GameId: args.GameId,
+                Type: args.Type,
+                ActorNr: args.ActorNr,
+                IsInactive: args.IsInactive
+            }
+        }
+    });
 };
 handlers["RoomLeft"] = RoomLeft;
 // Triggered automatically when a Photon room closes
 // Note: currentPlayerId is undefined in this function
 var RoomClosed = function (args) {
-    log.debug("Room Closed - Game: " + args.GameId);
+    server.WriteTitleEvent({
+        EventName: "room_closed",
+        Body: {
+            WebHook: {
+                AppVersion: args.AppVersion,
+                Region: args.Region,
+                GameId: args.GameId,
+                Type: args.Type,
+                ActorCount: args.ActorCount
+            }
+        }
+    });
 };
 handlers["RoomClosed"] = RoomClosed;
 // Triggered automatically when a Photon room game property is updated.
 var RoomPropertyUpdated = function (args) {
-    log.debug("Room Property Updated - Game: " + args.GameId);
+    if (args.Type === "Game") {
+        server.WritePlayerEvent({
+            EventName: "room_properties_updated",
+            PlayFabId: args.UserId,
+            Body: {
+                WebHook: {
+                    AppVersion: args.AppVersion,
+                    Region: args.Region,
+                    GameId: args.GameId,
+                    ActorNr: args.ActorNr,
+                    Properties: args.Properties
+                }
+            }
+        });
+    }
+    else { // "Actor"
+        server.WritePlayerEvent({
+            EventName: "player_roperties_updated",
+            PlayFabId: args.UserId,
+            Body: {
+                WebHook: {
+                    AppVersion: args.AppVersion,
+                    Region: args.Region,
+                    GameId: args.GameId,
+                    ActorNr: args.ActorNr,
+                    TargetActor: args.TargetActor,
+                    Properties: args.Properties
+                }
+            }
+        });
+    }
 };
 handlers["RoomPropertyUpdated"] = RoomPropertyUpdated;
 // Triggered by calling "OpRaiseEvent" on the Photon client. The "args.Data" property is 
 // set to the value of the "customEventContent" HashTable parameter, so you can use
 // it to pass in arbitrary data.
 var RoomEventRaised = function (args) {
+    server.WritePlayerEvent({
+        EventName: "event_raised",
+        PlayFabId: args.UserId,
+        Body: {
+            WebHook: {
+                AppVersion: args.AppVersion,
+                Region: args.Region,
+                GameId: args.GameId,
+                ActorNr: args.ActorNr,
+                EvCode: args.EvCode
+            }
+        }
+    });
     var eventData = args.Data;
-    log.debug("Event Raised - Game: " + args.GameId + " Event Type: " + eventData.eventType);
     switch (eventData.eventType) { // use args.EvCode instead of embedding eventType in payload
         case "playerMove":
             processPlayerMove(eventData);
--- DefaultCloudScript.ts
+++ DefaultCloudScript.ts
@@ -274,32 +274,109 @@ handlers["unlockHighSkillContent"] = UnlockHighSkillContent;
 
 // Triggered automatically when a Photon room is first created
 var RoomCreated = function (args): void {
-    log.debug("Room Created - Game: " + args.GameId + " MaxPlayers: " + args.CreateOptions.MaxPlayers);
+    server.WritePlayerEvent({
+        EventName : "room_created",
+        PlayFabId: args.UserId,
+        Body: {
+            WebHook: {
+                AppVersion: args.AppVersion,
+                Region: args.Region,
+                GameId: args.GameId,
+                Type: args.Type,
+                ActorNr: args.ActorNr,
+                CreateOptions: args.CreateOptions
+            }
+        }
+    });
 }
 handlers["RoomCreated"] = RoomCreated;
 
 // Triggered automatically when a player joins a Photon room
 var RoomJoined = function (args): void {
-    log.debug("Room Joined - Game: " + args.GameId + " PlayFabId: " + args.UserId);
+    server.WritePlayerEvent({
+        EventName: "room_joined",
+        PlayFabId: args.UserId,
+        Body: {
+            WebHook: {
+                AppVersion: args.AppVersion,
+                Region: args.Region,
+                GameId: args.GameId,
+                ActorNr: args.ActorNr
+            }
+        }
+    });
 }
 handlers["RoomJoined"] = RoomJoined;
 
 // Triggered automatically when a player leaves a Photon room
 var RoomLeft = function (args): void {
-    log.debug("Room Left - Game: " + args.GameId + " PlayFabId: " + args.UserId);
+    server.WritePlayerEvent({
+        EventName: "room_left",
+        PlayFabId: args.UserId,
+        Body: {
+            WebHook: {
+                AppVersion: args.AppVersion,
+                Region: args.Region,
+                GameId: args.GameId,
+                Type: args.Type,
+                ActorNr: args.ActorNr,
+                IsInactive: args.IsInactive
+            }
+        }
+    });
 }
 handlers["RoomLeft"] = RoomLeft;
 
 // Triggered automatically when a Photon room closes
 // Note: currentPlayerId is undefined in this function
 var RoomClosed = function (args): void {
-    log.debug("Room Closed - Game: " + args.GameId);
+    server.WriteTitleEvent({
+        EventName: "room_closed",
+        Body: {
+            WebHook: {
+                AppVersion: args.AppVersion,
+                Region: args.Region,
+                GameId: args.GameId,
+                Type: args.Type,
+                ActorCount: args.ActorCount
+            }
+        }
+    });
 }
 handlers["RoomClosed"] = RoomClosed;
 
 // Triggered automatically when a Photon room game property is updated.
 var RoomPropertyUpdated = function (args): void {
-    log.debug("Room Property Updated - Game: " + args.GameId);
+    if (args.Type === "Game") {
+        server.WritePlayerEvent({
+            EventName: "room_properties_updated",
+            PlayFabId: args.UserId,
+            Body: {
+                WebHook: {
+                    AppVersion: args.AppVersion,
+                    Region: args.Region,
+                    GameId: args.GameId,
+                    ActorNr: args.ActorNr,
+                    Properties: args.Properties
+                }
+            }
+        });
+    } else { // "Actor"
+        server.WritePlayerEvent({
+            EventName: "player_roperties_updated",
+            PlayFabId: args.UserId,
+            Body: {
+                WebHook: {
+                    AppVersion: args.AppVersion,
+                    Region: args.Region,
+                    GameId: args.GameId,
+                    ActorNr: args.ActorNr,
+                    TargetActor: args.TargetActor,
+                    Properties: args.Properties
+                }
+            }
+        });
+    }
 }
 handlers["RoomPropertyUpdated"] = RoomPropertyUpdated;
 
@@ -307,9 +384,21 @@ handlers["RoomPropertyUpdated"] = RoomPropertyUpdated;
 // set to the value of the "customEventContent" HashTable parameter, so you can use
 // it to pass in arbitrary data.
 var RoomEventRaised = function (args): void {
-    var eventData = args.Data;
-    log.debug("Event Raised - Game: " + args.GameId + " Event Type: " + eventData.eventType);
+    server.WritePlayerEvent({
+        EventName: "event_raised",
+        PlayFabId: args.UserId,
+        Body: {
+            WebHook: {
+                AppVersion: args.AppVersion,
+                Region: args.Region,
+                GameId: args.GameId,
+                ActorNr: args.ActorNr,
+                EvCode: args.EvCode
+            }
+        }
+    });
 
+    var eventData = args.Data;
     switch (eventData.eventType) { // use args.EvCode instead of embedding eventType in payload
         case "playerMove":
             processPlayerMove(eventData);
